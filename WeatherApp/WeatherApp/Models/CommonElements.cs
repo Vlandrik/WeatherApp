74096a46fb912e25fb6bcaedcc70e9c73a1e4892
Vlandrik <vlandrik@gmail.com>
2018-07-04 14:10:54 +0300

    Add the project

---
+++ WeatherApp/WeatherApp/Models/CommonElements.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherApp.Models
+{
+    // Classes, commin for both of models
+
+    public class Main
+    {
+        public double Temp { private get; set; }
+        public double Temp_min { private get; set; }
+        public double Temp_max { private get; set; }
+
+        public string Temperature
+        {
+            get { return $"{Temp.ToString()} \u00B0C"; }
+        }
+        public string MinTemp
+        {
+            get { return $"{Temp_min.ToString()} \u00B0C"; }
+        }
+        public string MaxTemp
+        {
+            get { return $"{Temp_max.ToString()} \u00B0C"; }
+        }
+
+    }
+    public class Clouds
+    {
+        public double All { private get; set; }
+        public string Cloudiness
+        {
+            get { return $"{All.ToString()} %"; }
+        }
+    }
+
+    public class Weather
+    {
+        public string Main { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Wind
+    {
+        public double Speed { private get; set; }
+        public string WindSpeed
+        {
+            get { return $"{Speed.ToString()} m/s"; }
+        }
+
+    }
+
+}
---
+++ WeatherApp/WeatherApp/Models/CurrentWeather.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeatherApp.Models;
+
+namespace WeatherWebApp.Models
+{
+    /// <summary>
+    /// Model for current weather json object
+    /// </summary>
+    public class CurrentWeather
+    {
+        /// <summary>
+        /// Root of current weather json object
+        /// </summary>
+        public class Root
+        {
+            public Main Main { get; set; }
+            public string Name { get; set; }
+
+            /// <summary>
+            ///  dt storage json dt in millisecounds
+            /// </summary>
+            /// <value>dt property sets the value of the json dt</value>
+            public double Dt
+            {
+                set; private get;
+            }
+
+            /// <summary>
+            ///  date property represents json dt in ISO format yyyy-MM-dd hh:mm
+            /// </summary>
+            /// <value>dt property gets the value of the json dt</value>
+            public string Date
+            {
+                get
+                {
+                    double milliSecounds = Dt;
+                    DateTime day = new System.DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).ToLocalTime();
+                    day = day.AddSeconds(milliSecounds).ToLocalTime();
+                    return day.ToString("yyyy-MM-dd hh:mm");
+                }
+            }
+            public Wind Wind { get; set; }
+            public Clouds Clouds { get; set; }
+            public List<Weather> Weather { get; set; }
+        }
+    }
+}
